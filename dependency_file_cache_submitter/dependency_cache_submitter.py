3880e80127d4b8f67ed0d5c5a9d85b8ae2e15970
Chandrakanth Santharam <chandrakanth.162@gmail.com>
2023-08-28 19:16:34 +0530

    Update dependency_cache_submitter.py

--- dependency_file_cache_submitter/dependency_cache_submitter.py
+++ dependency_file_cache_submitter/dependency_cache_submitter.py
@@ -8,14 +8,18 @@
 # Submission Methodology:
 # -----------------------
 # A copy of hip file saved under users temp directory. The Submission script submit custom 
-# file cache nodes one-by-one from top-to-bottom from these file . 
-# Each file_cache node submitted as single deadline job and Each job dispatched with 
+# file cache nodes one-by-one from top-to-bottom from these file to the deadline farm. 
+# Each file_cache node submitted as a single deadline job and Each job dispatched with 
 # the 'Post Job Script' to the dealine farm, which ideally run at the end of the job once all 
 # the frames completes the caches. Second job depends First. Third job depends Second. etc..,,
-# The 'Post Job Script' is a python file run from native deadline python plugin,
+# The 'Post Job Script' is a python file runs at last from the native deadline python plugin,
 # In-charge of executing another python file from the hython standalone to open the hip file
 # switch the current custom file_cache 'load from disk' and save the hip file back again. 
-# With the new settings
+# 
+# Example
+# -------
+#
+# Consider a file contain 
 
 import hou
 import os 
